1f3932ec9b5b51610d5d7f7378625aa9894a1d20
kukimik <kukimik@poczta.onet.pl>
2021-01-19 15:35:28 +0100

    work in progress..

--- recon.sql
+++ recon.sql
@@ -1,71 +1,20 @@
-CREATE TABLE recon_settings
- (
-  main_package_name VARCHAR2(30) DEFAULT 'R_' NOT NULL
- ,types_package_name VARCHAR2(30) DEFAULT 'T_' NOT NULL
- ,default_schema VARCHAR2(30)
- ,rowtype_schema_param_name VARCHAR2(30) DEFAULT 'SCHEMA__' NOT NULL
- ,rowtype_defaults_param_name VARCHAR2(30) DEFAULT 'DEFAULTS__' NOT NULL
- ,rowtype_default_defaults VARCHAR2(30) DEFAULT 'NULL' NOT NULL
- ,record_schema_param_name VARCHAR2(30) DEFAULT 'SCHEMA__' NOT NULL
- ,record_package_param_name VARCHAR2(30) DEFAULT 'PACKAGE__' NOT NULL
- ,record_defaults_param_name VARCHAR2(30) DEFAULT 'DEFAULTS__' NOT NULL
- ,record_default_package VARCHAR2(30)
- ,record_default_defaults VARCHAR2(30) DEFAULT 'NULL'
- ,assarray_schema_param_name VARCHAR2(30) DEFAULT 'SCHEMA__' NOT NULL
- ,assarray_package_param_name VARCHAR2(30) DEFAULT 'PACKAGE__' NOT NULL
- ,assarray_default_package VARCHAR2(30)
- ,assarray_max_items NUMBER(3) DEFAULT 128 NOT NULL
- ,CONSTRAINT CK_recon_settings CHECK
-    (
-          rowtype_default_values_mode IN ('NONE','NULL','DEFAULT_AND_NONE','DEFAULT_AND_NULL')
-      AND records_default_values_mode IN ('NONE','NULL','DEFAULT_AND_NONE','DEFAULT_AND_NULL')
-      AND asarray_max_items BETWEEN 1 AND 1000
-    )
- );
-
-COMMENT ON TABLE recon_settings IS '';
-COMMENT ON COLUMN recon_settings.main_package_name IS '';
-COMMENT ON COLUMN recon_settings.types_package_name IS '';
-COMMENT ON COLUMN recon_settings.rowtype_schema_param_name IS '';
-COMMENT ON COLUMN recon_settings.rowtype_default_schema IS '';
-COMMENT ON COLUMN recon_settings.rowtype_default_values_mode IS '';
-COMMENT ON COLUMN recon_settings.records_schema_param_name IS '';
-COMMENT ON COLUMN recon_settings.records_package_param_name IS '';
-COMMENT ON COLUMN recon_settings.records_default_schema IS '';
-COMMENT ON COLUMN recon_settings.records_default_package IS '';
-COMMENT ON COLUMN recon_settings.records_default_values_mode IS '';
-COMMENT ON COLUMN recon_settings.asarray_schema_param_name IS '';
-COMMENT ON COLUMN recon_settings.asarray_package_param_name IS '';
-COMMENT ON COLUMN recon_settings.asarray_default_schema IS '';
-COMMENT ON COLUMN recon_settings.asarray_default_package IS '';
-
-CREATE TABLE recon_tables
- (
-  table_owner VARCHAR2(32) NOT NULL
- ,table_name VARCHAR2(32) NOT NULL
- ,CONSTRAINT PK_recon_tables PRIMARY KEY (table_owner, table_name) -- unnamed constraint possible?
- );
-
-COMMENT ON TABLE recon_tables IS '';
-COMMENT ON COLUMN recon_tables.table_owner IS '';
-COMMENT ON COLUMN recon_tables.table_name IS '';
-
 CREATE OR REPLACE PACKAGE recon
 IS
   ex_table_does_not_exist EXCEPTION;
   PRAGMA EXCEPTION_INIT (ex_table_does_not_exist,  -20501);
 
-  PROCEDURE add_table
-    (
-     p_table_owner recon_tables.table_owner%type DEFAULT sys_context('userenv', 'current_schema')
-    ,p_table_name recon_tables.table_name%type
-    );
+  -- type that describes possible default value "modes"
+  -- (i.e. does the constructor have default values for fields and if so, what are these values?)
+  SUBTYPE t_mode IS POSITIVE RANGE 1..4;
 
-  PROCEDURE remove_table
-    (
-     p_table_owner recon_tables.table_owner%type DEFAULT sys_context('userenv', 'current_schema')
-    ,p_table_name recon_tables.table_name%type
-    );
+  -- number of items that in an associative array that a constructor can initialize
+  SUBTYPE t_num_items_assarray IS POSITIVEN RANGE 1..1000;
+
+  -- constants, implemented as functions to avoid trouble with referencing package constants from other sites
+  FUNCTION mode_no_defaults             RETURN recon.t_mode DETERMINISTIC IS RETURN 1; END mode_no_defaults;
+  FUNCTION mode_null_defaults           RETURN recon.t_mode DETERMINISTIC IS RETURN 2; END mode_null_defaults;
+  FUNCTION mode_data_defaults           RETURN recon.t_mode DETERMINISTIC IS RETURN 3; END mode_data_defaults;
+  FUNCTION mode_data_defaults_and_nulls RETURN recon.t_mode DETERMINISTIC IS RETURN 4; END mode_data_defaults_and_nulls;
 
   PROCEDURE make_packages
     (
@@ -78,57 +27,81 @@ END recon;
 
 CREATE OR REPLACE PACKAGE BODY recon
 IS
-  TYPE t_table_column IS RECORD
-    (
-     column_name VARCHAR2(30)
-    ,data_default VARCHAR2(32767) -- can fail for very long data_defaults!
-    );
+  --------------------------------
+  -- Private types
+  --------------------------------
+    TYPE t_table_column IS RECORD
+      (
+       column_name VARCHAR2(30)
+      ,data_default VARCHAR2(32767) -- can fail for very long data_defaults!
+      );
 
-  TYPE t_table_columns IS TABLE OF recon.t_table_column;
+    TYPE t_table_columns IS TABLE OF recon.t_table_column;
 
-  TYPE t_function IS RECORD
-    (
-     function_head CLOB
-    ,function_body CLOB
-    );
+    TYPE t_function IS RECORD
+      (
+       function_head CLOB
+      ,function_body CLOB
+      );
 
-
-  PROCEDURE add_table
-    (
-     p_table_owner recon_tables.table_owner%type DEFAULT sys_context('userenv', 'current_schema')
-    ,p_table_name recon_tables.table_name%type
-    )
-  IS
-  BEGIN
-    INSERT INTO recon_tables( table_owner, table_name )
-    VALUES (p_table_owner, p_table_name);
-  END add_table;
-
-  PROCEDURE remove_table
-    (
-     p_table_owner recon_tables.table_owner%type DEFAULT sys_context('userenv', 'current_schema')
-    ,p_table_name recon_tables.table_name%type
-    )
-  IS
-  BEGIN
-    DELETE FROM recon_tables WHERE table_owner = p_table_owner AND table_name = p_table_name;
-  END remove_table;
+  --------------------------------
+  -- Public functions
+  --------------------------------
 
   PROCEDURE make_packages
     (
      p_package_name VARCHAR2 DEFAULT 'R_'
     ,p_types_package_name VARCHAR2 DEFAULT 'T_'
-    ,p_schema_param_name VARCHAR2 DEFAULT 'SCHEMA__'
     ,p_default_schema VARCHAR2 DEFAULT sys_context('userenv', 'current_schema')
+    ,p_schema_param_name VARCHAR2 DEFAULT 'SCHEMA__'
+    ,p_package_param_name VARCHAR2 DEFAULT 'PACKAGE__'
+    ,p_mode_param_name VARCHAR2 DEFAULT 'DEFAULTS__'
+    ,p_rowtype_default_mode recon.t_mode DEFAULT recon.mode_null_defaults -- perhaps no default mode should be provided
+    ,p_record_default_mode recon.t_mode DEFAULT recon.mode_data_defaults_and_nulls -- perhaps no default mode should be provided
+    ,p_assarray_max_items recon.t_num_items_assarray DEFAULT 128
     )
   IS
     c_package_name_enq CONSTANT VARCHAR2(32)
       := dbms_assert.simple_sql_name( dbms_assert.enquote_name(p_package_name) );
+    c_types_package_name_enq CONSTANT VARCHAR2(32)
+      := dbms_assert.simple_sql_name( dbms_assert.enquote_name(p_types_package_name) );
     c_schema_param_name_enq CONSTANT VARCHAR2(32)
       := dbms_assert.simple_sql_name( dbms_assert.enquote_name(p_schema_param_name) );
+    c_package_param_name_enq CONSTANT VARCHAR2(32)
+      := dbms_assert.simple_sql_name( dbms_assert.enquote_name(p_package_param_name) );
+    c_mode_param_name_enq CONSTANT VARCHAR2(32)
+      := dbms_assert.simple_sql_name( dbms_assert.enquote_name(p_mode_param_name) );
     c_default_schema_enq CONSTANT VARCHAR2(32)
       := dbms_assert.simple_sql_name( dbms_assert.enquote_name(p_default_schema) );
+    c_rowtype_default_mode CONSTANT recon.t_mode := p_rowtype_default_mode;
+    c_record_default_mode CONSTANT recon.t_mode := p_record_default_mode;
+    c_assarray_max_items CONSTANT recon.t_num_items_assarray := p_assarray_max_items;
+  BEGIN
+    IF c_assarray_max_items IS NULL
+    THEN
+      RAISE value_error;
+    END IF;
 
+    recon.make_types_package
+      (
+       p_types_package_name_enq => c_types_package_name_enq
+      ,p_default_schema_enq => c_default_schema_enq
+      );
+
+    recon.make_main_package
+      (
+       p_package_name_enq => c_package_name_enq
+      ,p_types_package_name_enq => c_types_package_name_enq
+      ,p_schema_param_name_enq => c_schema_param_name_enq
+      ,p_package_param_name_enq => c_package_param_name_enq
+      ,p_mode_param_name_enq => c_mode_param_name_enq
+      ,p_rowtype_default_mode => c_rowtype_default_mode
+      ,p_record_default_mode => c_record_default_mode
+      ,p_assarray_max_items => c_assarray_max_items
+      );
+  END make_packages;
+
+  DECLARE
     TYPE t_varchar2integer IS TABLE OF INTEGER INDEX BY VARCHAR2(32);
     v_schema2number t_varchar2integer;
 
@@ -136,18 +109,6 @@ IS
     v_sql_body CLOB;
     v_buffer CLOB;
   BEGIN
-    v_types_package_data :=
-      recon.make_types_package
-        (
-         p_types_package_name_enq => c_types_package_name_enq
-        ,p_default_schema_enq => c_default_schema_enq
-        );
-
-    recon.make_main_package
-      (
-       p_package_name_enq => c_package_name_enq
-      );
-  END make_packages;
 
     v_sql_head := 'CREATE OR REPLACE PACKAGE '||c_package_name||' IS'||chr(10);
     v_sql_body := 'CREATE OR REPLACE PACKAGE BODY '||c_package_name||' IS'||chr(10);
@@ -294,9 +255,9 @@ $END
               ||c_schema_param_name
               ||' '
               ||c_package_name
-              ||'.t_'
-              ||v_schema2number(rec.table_owner_enq)
-              ||':='
+              ||'.schemas.'
+              ||rec.table_owner_enq
+              ||'%type :='
               ||c_package_name
               ||'.schemas.'
               ||rec.table_owner_enq
